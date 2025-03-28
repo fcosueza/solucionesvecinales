65ee66473ff904b16e4dd36a1e4cec245c031b7e
Fco Sueza <fcosueza@disroot.org>
2025-03-28 10:05:00 +0100

    Added jest and configuring automation and integration with Next

---
+++ jest.config.ts
@@ -0,0 +1,15 @@
+import type { Config } from "jest";
+import nextJest from "next/jest";
+
+const createJestConfig = nextJest({
+  dir: "./"
+});
+
+const config: Config = {
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  coverageProvider: "v8",
+  testEnvironment: "jsdom"
+};
+
+export default createJestConfig(config);
