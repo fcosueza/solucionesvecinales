cb2feb940e148b34c9e0c215293284735cc02be2
Fco Sueza <fcosueza@disroot.org>
2025-04-18 12:49:53 +0200

    Added zigzag section with placeholder text and images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -43,6 +43,42 @@ export default function Home() {
             </h2>
           </div>
         </section>
+        <section id="zigzag" className={style.zigzag}>
+          <div id="zigzag-left" className={style.section}>
+            <Image
+              src="assets/images/section2-noti.svg"
+              alt="Imagen de la pantalla de un monitor"
+              width={500}
+              height={500}
+            />
+            <div className={style.zigzag__text}>
+              <h3 className={style.zigzag__title}>
+                Una app accesible y simple de usar por cualquiera
+              </h3>
+              <p className={style.zigzag__para}>
+                Con un interfaz sencilla y un diseño simple, Soluciones Vecinales puede ser usada
+                por cualquier usuario, independientemente de sus conocimientos informáticos. Además,
+                incluye un sistema de ayuda muy intuitivo.
+              </p>
+            </div>
+          </div>
+          <div id="zigzag-right" className={style.section}>
+            <div className={style.zigzag__text}>
+              <h3 className={style.zigzag__title}>Lorem Ipsum Dolor Sit Amet</h3>
+              <p className={style.zigzag__para}>
+                Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium
+                doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore
+                veritatis et quasi architecto beatae vitae dicta sunt explicabo
+              </p>
+            </div>
+            <Image
+              src="assets/images/section2-easy.svg"
+              alt="Imagen de la pantalla de un monitor"
+              width={500}
+              height={500}
+            />
+          </div>
+        </section>
       </main>
     </>
   );
