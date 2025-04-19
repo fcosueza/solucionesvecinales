0ed9450200a65eda73b2e13fa04024d3813d6350
Fco Sueza <fcosueza@disroot.org>
2025-04-19 20:14:48 +0200

    Added Card component

--- src/ui/components/Card/index.test.tsx
+++ src/ui/components/Card/index.test.tsx
@@ -2,9 +2,48 @@ import { render, screen } from "@testing-library/react";
 import Card from ".";
 
 describe("Tests del componente Card...", () => {
-  it("Debe renderizar un elemento Card", () => {
-    render(<Card />);
+  const imageURL = "/assets/image/doc.svg";
+  const altText = "Imagen de unos documentos";
+  const title = "Lorem Ipsum Dolor";
+  const para = "Lorem Ipsum Dolor sit atmet consecterum";
+
+  it("Debe renderizar un elemento de tipo Card", () => {
+    render(<Card imageURL={imageURL} imageAltText={altText} cardTitle={title} cardPara={para} />);
 
     expect(screen.getByRole("card")).toBeInTheDocument();
+  });
+
+  it("Debe renderizar un elemento de tipo Card con la imagen que hemos especificado", () => {
+    const imgWidth = 120;
+    const imgHeight = 200;
+
+    render(
+      <Card
+        imageURL={imageURL}
+        imageAltText={altText}
+        imageWidth={imgWidth}
+        imageHeight={imgHeight}
+        cardTitle={title}
+        cardPara={para}
+      />
+    );
+
+    expect(screen.getByRole("img")).toBeInTheDocument();
+    expect(screen.getByRole("img")).toHaveProperty("width", imgWidth);
+    expect(screen.getByRole("img")).toHaveProperty("height", imgHeight);
+  });
+
+  it("Debe renderizar un elemento de tipo Card con el titulo especificado", () => {
+    render(<Card imageURL={imageURL} imageAltText={altText} cardTitle={title} cardPara={para} />);
+
+    expect(screen.getByRole("heading")).toBeInTheDocument();
+    expect(screen.getByText(title)).toBeInTheDocument();
+  });
+
+  it("Debe renderizar un elemento de tipo Card con el párrafo especificado", () => {
+    render(<Card imageURL={imageURL} imageAltText={altText} cardTitle={title} cardPara={para} />);
+
+    expect(screen.getByRole("paragraph")).toBeInTheDocument();
+    expect(screen.getByText(para)).toBeInTheDocument();
   });
 });
--- src/ui/components/Card/index.tsx
+++ src/ui/components/Card/index.tsx
@@ -1,5 +1,29 @@
-const Card = () => {
-  return <div role="card"></div>;
+import Image from "next/image";
+
+interface Props {
+  imageURL: string;
+  imageAltText: string;
+  imageWidth?: number;
+  imageHeight?: number;
+  cardTitle: string;
+  cardPara: string;
+}
+
+const Card = ({
+  imageURL,
+  imageAltText,
+  imageWidth = 100,
+  imageHeight = 100,
+  cardTitle,
+  cardPara
+}: Props) => {
+  return (
+    <div role="card">
+      <Image src={imageURL} alt={imageAltText} width={imageWidth} height={imageHeight} />
+      <h3>{cardTitle}</h3>
+      <p>{cardPara}</p>
+    </div>
+  );
 };
 
 export default Card;
