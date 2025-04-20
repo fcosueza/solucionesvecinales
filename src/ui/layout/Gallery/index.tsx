b55b004edd5dd705906bf64486a8e7853ae9001d
Fco Sueza <fcosueza@disroot.org>
2025-04-20 11:14:08 +0200

    Changed section 1 and 2 iilustrations

--- src/ui/layout/Gallery/index.tsx
+++ src/ui/layout/Gallery/index.tsx
@@ -1,5 +1,5 @@
-const Gallery = () => {
-  return <div role="gallery"></div>;
+const Gallery = ({ children }: Readonly<{ children: React.ReactNode }>) => {
+  return <div role="gallery">{children}</div>;
 };
 
 export default Gallery;
