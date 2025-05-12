f93a9533e0b93964fcfffa8b6d98bcd237d4ae85
Fco Sueza <fcosueza@disroot.org>
2025-05-12 13:16:34 +0200

    Here we go again...

--- playwright.config.ts
+++ playwright.config.ts
@@ -68,12 +68,11 @@ export default defineConfig({
     //   name: 'Google Chrome',
     //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
     // },
-  ]
+  ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  // command: "npm run start",
-  // url: "http://localhos:3001",
-  // reuseExistingServer: !process.env.CI
-  //}
+  webServer: {
+    command: "npm run dev",
+    url: "http://127.0.0.1:3000"
+  }
 });
