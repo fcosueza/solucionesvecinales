04265819b30c86030ca0c16b9ab33c7c6376b462
Fco Sueza <fcosueza@disroot.org>
2025-08-04 14:09:29 +0200

    Updated some test for FormRadio component

--- src/ui/components/FormComp/FormRadio/index.test.tsx
+++ src/ui/components/FormComp/FormRadio/index.test.tsx
@@ -1,69 +1,65 @@
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
-import { FormRadioAttrs, RadioType } from "@/types";
+import { RadioType } from "@/types";
 import FormRadio from ".";
 
 describe("FormRadio component test suite...", () => {
   const legendTxt: string = "Testing Label";
   const radioType = RadioType.radio;
-  const attr: FormRadioAttrs[] = [
+  const elements = [
     {
-      id: "test1",
-      name: "test1",
       labelText: "Test1",
-      value: "test1"
+      radioAttr: {
+        id: "test1",
+        name: "test1",
+        value: "test1",
+        defaultChecked: true
+      }
     },
     {
-      id: "test2",
-      name: "test2",
-      labelText: "test2",
-      value: "test2"
+      labelText: "Test2",
+      radioAttr: {
+        id: "test2",
+        name: "test2",
+        value: "test2"
+      }
     }
   ];
 
-  it("Should render by default a label and an input correctly ", () => {
-    render(<FormRadio legend={legendTxt} attr={attr} type={radioType} />);
+  it("Should render the form control", () => {
+    render(<FormRadio legend={legendTxt} elementList={elements} type={radioType} />);
+
+    expect(screen.getByRole("form-control")).toBeInTheDocument();
+  });
+
+  it("Should render the legend with the proper text", () => {
+    render(<FormRadio legend={legendTxt} elementList={elements} type={radioType} />);
 
     expect(screen.getByLabelText(legendTxt)).toBeInTheDocument();
-    expect(screen.getByRole("textbox")).toBeInTheDocument();
-  });
-  it("Should render the label with the proper text", () => {
-    render(<FormRadio legend={legendTxt} attr={attr} type={radioType} />);
-    expect(screen.getByLabelText(legendTxt)).toBeInTheDocument();
   });
 
-  it("Should render the input with the proper field attributes", () => {
-    render(<FormRadio legend={legendTxt} attr={attr} type={radioType} />);
+  it("Should render inputs of the specieiged type", () => {
+    render(<FormRadio legend={legendTxt} elementList={elements} type={radioType} />);
 
-    const input = screen.getByRole("textbox");
-
-    expect(input).toHaveProperty("type", radioType);
-  });
-
-  it("Should render input with the specified name", () => {
-    const name = "TestName";
-
-    render(<FormRadio legend={legendTxt} attr={attr} type={radioType} />);
-
-    expect(screen.getByRole("textbox")).toHaveProperty("name", name);
+    expect(screen.getAllByRole(radioType)).toHaveLength(elements.length);
   });
 
   it("Should render a textarea with the default rows", () => {
     const defaultRows = 5;
 
-    render(<FormRadio legend={legendTxt} attr={attr} type={radioType} />);
+    render(<FormRadio legend={legendTxt} elementList={elements} type={radioType} />);
     expect(screen.getByRole("textbox")).toHaveProperty("rows", defaultRows);
   });
 
   it("Should render a textarea with the specified rows", () => {
     const rows = 10;
 
-    render(<FormRadio legend={legendTxt} attr={attr} type={radioType} />);
+    render(<FormRadio legend={legendTxt} elementList={elements} type={radioType} />);
     expect(screen.getByRole("textbox")).toHaveProperty("rows", rows);
   });
 
   it("Should show in input field what the user is writing", async () => {
-    render(<FormRadio legend={legendTxt} attr={attr} type={radioType} />);
+    render(<FormRadio legend={legendTxt} elementList={elements} type={radioType} />);
 
     const userInput = "testname";
     const nameInput = screen.getByRole("textbox");
@@ -76,7 +72,7 @@ describe("FormRadio component test suite...", () => {
   it("Should show in input field the specified value", async () => {
     const value = "Ipp";
 
-    render(<FormRadio legend={legendTxt} attr={attr} type={radioType} />);
+    render(<FormRadio legend={legendTxt} elementList={elements} type={radioType} />);
 
     expect(screen.getByRole("textbox")).toHaveValue(value);
   });
@@ -84,7 +80,7 @@ describe("FormRadio component test suite...", () => {
   it("Should render input with the required field to false by default", () => {
     const name = "TestName";
 
-    render(<FormRadio legend={legendTxt} attr={attr} type={radioType} />);
+    render(<FormRadio legend={legendTxt} elementList={elements} type={radioType} />);
 
     expect(screen.getByRole("textbox")).toHaveProperty("name", name);
   });
@@ -92,7 +88,7 @@ describe("FormRadio component test suite...", () => {
   it("Should show an error msg if there is one", async () => {
     const errorMsg = "testerror";
 
-    render(<FormRadio legend={legendTxt} attr={attr} type={radioType} errorMsg={errorMsg} />);
+    render(<FormRadio legend={legendTxt} elementList={elements} type={radioType} errorMsg={errorMsg} />);
 
     const errorElement = screen.getByRole("alert");
 
