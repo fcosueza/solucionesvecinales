e0e05152709e38b02afc0743d29c005ac97efe12
Fco Sueza <fcosueza@disroot.org>
2025-08-04 13:49:58 +0200

    Added basic FormRadio component behavior

--- src/ui/components/FormComp/FormRadio/index.tsx
+++ src/ui/components/FormComp/FormRadio/index.tsx
@@ -1,14 +1,40 @@
 import { FormRadioAttrs, RadioType } from "@/types";
+import style from "./style.module.css";
+import FormError from "../FormError";
 
 interface Props {
   legend: string;
   type: RadioType;
-  attr: [{ labelText: string; radioAttr: FormRadioAttrs[] }];
+  elementList: { labelText: string; radioAttr: FormRadioAttrs }[];
   errorMsg?: string | string[];
 }
 
-const FormRadio = ({ legend, type, attr, errorMsg = "" }: Props): React.ReactNode => {
-  return <h1>Radio suite</h1>;
+// TODO: Implement checkbox type generation.
+
+const FormRadio = ({ legend, type, elementList, errorMsg = "" }: Props): React.ReactNode => {
+  if (type == RadioType.checkbox) return <h1>checkbox not implemented yet!!</h1>;
+
+  const items: React.ReactNode = elementList.map(element => (
+    <div className={style.radioCont} key={`${element.radioAttr.id}`}>
+      <input
+        type={type}
+        className={style.radio}
+        aria-label={`${element.radioAttr.id}-${type}`}
+        {...element.radioAttr}
+      />
+      <label htmlFor="tenant">inquilino</label>
+    </div>
+  ));
+
+  return (
+    <div className={style.control}>
+      <fieldset className={style.fieldset} role="radiogroup">
+        <legend className={style.legend}>{legend}</legend>
+        {items}
+        {errorMsg ? <FormError message={errorMsg} /> : ""}
+      </fieldset>
+    </div>
+  );
 };
 
 export default FormRadio;
