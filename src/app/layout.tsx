742fd8704f63a1e36782c2227ca04e4b66f7ec7c
Fco Sueza <fcosueza@disroot.org>
2025-08-07 13:49:52 +0200

    Using metadata Next object instead meta tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,18 @@
 import "../ui/global.css";
+import type { Metadata } from "next";
 import { Roboto } from "next/font/google";
 
 const roboto = Roboto({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Soluciones Vecinales",
+  description: "Aplicación para la gestión de comunidades de vecinos",
+  themeColor: "#000000",
+  authors: [{ name: "Francisco Sueza Rodríguez" }],
+  icons: {
+    icon: "favicon.ico"
+  }
+};
 
 export default function RootLayout({
   children
@@ -10,18 +21,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={roboto.className}>
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="theme-color" content="#000000" />
-        <meta name="author" content="Francisco Sueza Rodríguez" />
-        <meta name="description" content="Aplicación para la gestión de comunidades de vecinos" />
-
-        <link rel="icon" href="favicon.ico" />
-
-        <title>Soluciones Vecinales</title>
-      </head>
-
       <body>{children}</body>
     </html>
   );
