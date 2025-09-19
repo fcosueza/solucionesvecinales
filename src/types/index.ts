e52cc8aaea57f64272089c208279ad1a80c4f87f
Fco Sueza <fcosueza@disroot.org>
2025-09-19 11:09:55 +0200

    Addded SessionPayload basic type (it will be expanded as needed)

--- src/types/index.ts
+++ src/types/index.ts
@@ -59,3 +59,8 @@ export interface FormRadioAttrs extends FormFieldAttrs {
   value: string | number | undefined;
   defaultChecked?: boolean;
 }
+
+export interface SessionPayload {
+  userID: string;
+  role: UserRole;
+}
