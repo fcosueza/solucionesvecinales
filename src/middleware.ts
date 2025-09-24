9198229d535f543271cb3649add455ad9d49949c
Fco Sueza <fcosueza@disroot.org>
2025-09-24 10:04:36 +0200

    Correted missing dash in route and redirect route not well formed

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 import { decryptSession } from "./lib/session";
 import { cookies } from "next/headers";
 
-const protecetedRoutes = ["dashboard"];
+const protecetedRoutes = ["/dashboard"];
 const publicRoutes = ["/home", "/login", "/signin"];
 
 async function middleware(req: NextRequest): Promise<NextResponse> {
@@ -14,7 +14,7 @@ async function middleware(req: NextRequest): Promise<NextResponse> {
   const session = await decryptSession(cookie);
 
   if (isProtectedRoute && "error" in session) {
-    return NextResponse.redirect("/login");
+    return NextResponse.redirect(new URL("/login", req.nextUrl));
   }
 
   if (isPublicRoute && "userID" in session && !req.nextUrl.pathname.startsWith("/dashboard")) {
