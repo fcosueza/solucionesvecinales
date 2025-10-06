97bedde88e5fd78d3a2fe2c7ce9cf7430524fc98
Fco Sueza <fcosueza@disroot.org>
2025-10-06 13:42:23 +0200

    Added logOutAction test

--- src/actions/auth/logOutAction.test.ts
+++ src/actions/auth/logOutAction.test.ts
@@ -1,3 +1,20 @@
 import logOutAction from "./logOutAction";
 import { deleteSession } from "@/lib/session";
 import { redirect } from "next/navigation";
+
+jest.mock("next/navigation");
+jest.mock("@/lib/session");
+
+describe("logOutAction test suite...", () => {
+  it("Should call deleteSession function", async () => {
+    await logOutAction();
+
+    expect(deleteSession).toHaveBeenCalled();
+  });
+
+  it("Should redirect the user to home page", async () => {
+    await logOutAction();
+
+    expect(redirect).toHaveBeenCalledWith("/");
+  });
+});
