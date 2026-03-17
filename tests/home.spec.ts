d7aebdc621f15a24c5e768f0175117a29fe7666c
Fco Sueza <fcosueza@disroot.org>
2026-03-17 19:30:43 +0100

    Update home playwright test title (only translation)

--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -1,12 +1,12 @@
 import { test, expect } from "@playwright/test";
 
-test("La página tiene el título adecuado", async ({ page }) => {
+test("The home page has the correct title", async ({ page }) => {
   await page.goto("http://localhost:3000");
 
   await expect(page).toHaveTitle(/Soluciones Vecinales/i);
 });
 
-test("La página tiene dos logos", async ({ page }) => {
+test("The home page has two logos", async ({ page }) => {
   await page.goto("http://localhost:3000");
 
   const images = await page.getByRole("img", { name: /Logo/i }).all();
