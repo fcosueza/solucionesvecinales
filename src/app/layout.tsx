6a4244f6eb7f70fa465fd6899d884d1f7285f6a5
Fco Sueza <fcosueza@disroot.org>
2026-05-11 06:44:42 +0200

    fix: added supression for hydration error in layout.tsx to prevent hydration mismatch error in development mode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,8 +29,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="es" className={inter.className}>
-      <body>
+    <html lang="es" className={inter.className} suppressHydrationWarning>
+      <body suppressHydrationWarning>
         {children}
         <SonnerToaster />
       </body>
