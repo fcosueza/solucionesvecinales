e8257131a94279b83cab5c6e6202c25d989e638f
Fco Sueza <fcosueza@disroot.org>
2026-06-24 21:41:32 +0200

    fix: fixed unmatching e2e test form messages after trasnlations.

--- src/schemas/auth/login.schema.ts
+++ src/schemas/auth/login.schema.ts
@@ -1,11 +1,7 @@
 import { z } from "zod";
 
 const logInSchema = z.object({
-  email: z
-    .string()
-    .email({ message: "El correo no es válido" })
-    .min(1, { message: "El email es obligatorio" })
-    .trim(),
+  email: z.string().email({ message: "El correo no es válido" }).min(1, { message: "El email es obligatorio" }).trim(),
   password: z
     .string({ message: "La contraseña no es válida" })
     .min(15, { message: "La contraseña tiene que tener 15 caracteres min" })
